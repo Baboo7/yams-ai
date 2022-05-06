574807f402b372a6a298e083838aed90d26bf367
Baboo7 <dbsharp404@gmail.com>
2022-05-06 12:18:05 -0700

    chore: Clean dumb player

--- player_dumb.py
+++ player_dumb.py
@@ -1,11 +1,6 @@
-from config import DICES_COUNT
-from utils import pick_rand, rand_int
+from utils import pick_rand
 
 
 class PlayerDumb():
     def pick_action(self, actions: list[str]) -> str:
         return pick_rand(actions)
-
-    def pick_dices(self, dices):
-        pick_count = rand_int(1, DICES_COUNT)
-        return dices[0:pick_count - 1]
